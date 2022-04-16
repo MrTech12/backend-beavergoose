195e43a04d0839ac08376396f458202dad4ab79b
Sina Nazar Soroush <13730935+MrTech12@users.noreply.github.com>
2022-04-16 10:38:54 +0200

    Add: method to LinkService service class to trigger workflow

--- src/Services/LinkService/Services/CreationService.cs
+++ src/Services/LinkService/Services/CreationService.cs
@@ -7,6 +7,11 @@
             return number1 + number2;
         }
 
+        public int SubtractNumbers(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
         public CreationService()
         {
 
