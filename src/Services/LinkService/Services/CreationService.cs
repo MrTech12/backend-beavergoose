83a5abc28cfe19ca738bf6c275869f05dda173bb
Sina Nazar Soroush <13730935+MrTech12@users.noreply.github.com>
2022-04-16 13:04:06 +0200

    Add: find location of code coverage files part 2

--- src/Services/LinkService/Services/CreationService.cs
+++ src/Services/LinkService/Services/CreationService.cs
@@ -7,9 +7,9 @@
             return number1 + number2;
         }
 
-        public int DivideNumbers(int number1, int number2)
+        public int SubtractNumbers(int number1, int number2)
         {
-            return number1 / number2;
+            return number1 - number2;
         }
 
         public CreationService()
