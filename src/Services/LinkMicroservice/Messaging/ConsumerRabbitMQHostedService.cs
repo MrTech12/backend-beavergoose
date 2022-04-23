f799c3bd9e810e537f074566f8a1ff62f18ee453
Sina Nazar Soroush <13730935+MrTech12@users.noreply.github.com>
2022-04-23 12:41:00 +0200

    Edit: logic for ConsumerRabbitMQHostedService
    * Added exchange & routingKeys, for better organization of the sended
    messages.

--- src/Services/LinkMicroservice/Messaging/ConsumerRabbitMQHostedService.cs
+++ src/Services/LinkMicroservice/Messaging/ConsumerRabbitMQHostedService.cs
@@ -12,7 +12,8 @@ namespace LinkMicroservice.Messaging
         private ConnectionFactory _connectionFactory;
         private IConnection _connection;
         private IModel _channel;
-        private const string QueueName = "link-creation";
+        private const string queueName = "link.creation";
+        private const string exchangeName = "link-exchange";
         private readonly IConfiguration _configuration;
 
         public ConsumerRabbitMQHostedService(IConfiguration configuration, ILogger<ConsumerRabbitMQHostedService> logger)
@@ -26,19 +27,21 @@ namespace LinkMicroservice.Messaging
             this._connectionFactory = new ConnectionFactory
             {
                 HostName = this._configuration["RabbitMQ:HostName"],
-                Port = Convert.ToInt32(this._configuration["RabbitMQ:Port"])
+                Port = Convert.ToInt32(this._configuration["RabbitMQ:Port"]),
+                DispatchConsumersAsync = true
             };
             this._connection = _connectionFactory.CreateConnection();
             this._channel = _connection.CreateModel();
 
-            this._channel.ExchangeDeclare("link-exchange", ExchangeType.Topic);
-            this._channel.QueueDeclare(queue: QueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            this._channel.ExchangeDeclare(exchangeName, ExchangeType.Topic);
+            this._channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            this._channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: "link.*", arguments: null);
 
             var properties = this._channel.CreateBasicProperties();
             properties.Persistent = true; // Declaring the message as persistent
             this._channel.BasicQos(0, 1, false); // Send messages to different workers based on received acknowledgment
 
-            this._logger.LogInformation($"Queue [{QueueName}] is waiting for messages.");
+            this._logger.LogInformation($"Queue [{queueName}] is waiting for messages.");
             return base.StartAsync(cancellationToken);
         }
 
@@ -54,11 +57,11 @@ namespace LinkMicroservice.Messaging
                 var fileDiscoveryDto = JsonSerializer.Deserialize<FileDiscoveryDTO>(content);
 
                 await HandleMessage();
-                _channel.BasicAck(ea.DeliveryTag, false); // Letting RabbitMQ know that the message had been received.
+                _channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false); // Letting RabbitMQ know that the message had been received.
             };
 
 
-            this._channel.BasicConsume(queue: "hello", autoAck: false, consumer: consumer);
+            this._channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
             
             await Task.CompletedTask;
         }
