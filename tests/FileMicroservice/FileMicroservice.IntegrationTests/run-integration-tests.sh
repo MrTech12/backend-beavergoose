45256e4ea3b0b691645fcf2f08b3f921b353b034
Sina Nazar Soroush <13730935+MrTech12@users.noreply.github.com>
2022-04-24 17:57:31 +0200

    Change: bash script to configure the AWS CLI
    * Added commands to configure basic properies
    * Added check to see status of Localstack
    * Added filemicroservice workflow & workflow template for integration
    tests.

--- tests/FileMicroservice/FileMicroservice.IntegrationTests/run-integration-tests.sh
+++ tests/FileMicroservice/FileMicroservice.IntegrationTests/run-integration-tests.sh
@@ -7,37 +7,54 @@
 # 4. Configure an S3 bucket
 # 4. Run the Integration tests
 
-who="qwerty"
-printf "Hello, $who! \n\n"
-
 printf "Executing helper script for running Integration tests. \n"
-
 printf "\n"
+
+printf "Going to the working directory of the Integration tests. \n"
+cd $1
+printf '\n'
+
+printf "New working directory: "
+echo $PWD
+printf '\n'
 
 printf "Running Docker compose file to pull up & boot up Localstack container. \n"
 printf "Command that is executed: 'docker-compose up' \n"
-# echo
+docker-compose up
 printf "\n"
 
-printf "Waiting for Localstack to be pulled down & to boot up... \n"
-printf "Command that is executed: 'sleep 30' \n"
-sleep 5s
+printf "Waiting for Localstack to be pulled down & to boot up, for 4 minutes \n"
+printf "Command that is executed: 'sleep 4m' \n"
+sleep 4m
 printf "\n"
 
 printf "Making HTTP requests to check the status of Localstack.\n"
 printf "Command that is executed: 'curl http:/localhost:4566' \n"
-# running curl
+
+RESULT=$(curl http://localhost:4566/)
+if [[ "$RESULT" == *"running"* ]]; then
+  printf "localstack is running \n"
+fi
+printf "\n"
+
+printf "Configuring the AWS CLI with dummy data. \n"
+printf "Command one that is executed: aws configure set aws_access_key_id AAAAA"
+printf "Command two that is executed: aws configure set aws_secret_access_key AAAAA"
+printf "Command three that is executed: aws configure set default.region us-west-2"
+aws configure set aws_access_key_id AAAAA
+aws configure set aws_secret_access_key AAAAA
+aws configure set default.region us-west-2
 printf "\n"
 
 printf "Localstack is running, now configuring a new S3 Bucket. \n"
 printf "Command one that is executed: 'aws --endpoint-url=http://localhost:4566 s3 mb s3://test-bucket' \n"
-# echo
+aws --endpoint-url=http://localhost:4566 s3 mb s3://test-bucket
 printf "\n"
 printf "Command two that is executed: 'aws --endpoint-url=http://localhost:4566 s3api put-bucket-acl --bucket test-bucket --acl public-read' \n" 
-# echo
+aws --endpoint-url=http://localhost:4566 s3api put-bucket-acl --bucket test-bucket --acl public-read
 printf "\n"
 
 printf "The environment is ready. Time to run the integration tests. \n"
-printf "Command that executed: {dotnet test $1 --logger 'trx;LogFileName=test-results.trx' --collect:'XPlat Code Coverage'} \n"
-# echo
+printf "Command that executed: {dotnet test --logger 'trx;LogFileName=test-results.trx' --collect:'XPlat Code Coverage'} \n"
+dotnet test --logger "trx;LogFileName=test-results.trx" --collect:"XPlat Code Coverage"
 printf "\n"
