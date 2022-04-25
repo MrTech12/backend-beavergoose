0ba9eefadba19210da04aad68ea04003d8a16534
Sina Nazar Soroush <13730935+MrTech12@users.noreply.github.com>
2022-04-25 22:57:34 +0200

    Change: workflow in shell script integration tests
    * Changed how the scripts waits on a localstack container to boot for
    integration tests
    * Added 'until' loop to check the log of the container.
    * When the log has the suitable log output, the script will continue.
    * Added extra info to better differentiate between info messages
    from the script and messages from various programs.

--- tests/FileMicroservice/FileMicroservice.IntegrationTests/run-integration-tests.sh
+++ tests/FileMicroservice/FileMicroservice.IntegrationTests/run-integration-tests.sh
@@ -7,54 +7,56 @@
 # 4. Configure an S3 bucket
 # 4. Run the Integration tests
 
-printf "Executing helper script for running Integration tests. \n"
-printf "\n"
+printf "Message: Executing helper script for running Integration tests. \n\n"
 
-printf "Going to the working directory of the Integration tests."
+printf "Message: Going to the working directory of the Integration tests."
 cd $1
 printf '\n'
 
-printf "New working directory: "
+printf "Message: New working directory: "
 echo $PWD
 printf '\n'
 
-printf "Running Docker compose file to pull up & boot up Localstack container. \n"
-printf "Command that is executed: 'docker-compose up -d' \n"
+printf "Message: Running Docker compose file to pull up & boot up Localstack container. \n"
+printf "Message: Command that is executed: 'docker-compose up -d' \n"
 docker-compose up -d
 printf "\n"
 
-printf "Waiting for Localstack to be pulled down & to boot up, for 4.5 minutes \n"
-printf "Command that is executed: 'sleep 4.5m' \n"
-sleep 4.5m
+printf "Message: Waiting for Localstack to boot up \n"
+printf "Message: Command that is executed: 'docker logs localstack' \n"
+until [[ $(docker logs localstack) == *"Ready."* ]]
+do
+  printf "Message: still waiting on Localstack to boot..."
+  sleep 50s
+done
 printf "\n"
 
-printf "Making HTTP requests to check the status of Localstack.\n"
-printf "Command that is executed: 'curl http:/localhost:4566' \n"
-
+printf "Message: Making HTTP requests to check the status of Localstack.\n"
+printf "Message: Command that is executed: 'curl http:/localhost:4566' \n"
 RESULT=$(curl http://localhost:4566/)
 if [[ "$RESULT" == *"running"* ]]; then
   printf "localstack is running \n"
 fi
 printf "\n"
 
-printf "Configuring the AWS CLI with dummy data. \n"
-printf "Command one that is executed: aws configure set aws_access_key_id AAAAA \n"
-printf "Command two that is executed: aws configure set aws_secret_access_key AAAAA \n"
-printf "Command three that is executed: aws configure set default.region us-west-2 \n"
+printf "Message: Configuring the AWS CLI with dummy data. \n"
+printf "Message: Command one that is executed: aws configure set aws_access_key_id AAAAA \n"
+printf "Message: Command two that is executed: aws configure set aws_secret_access_key AAAAA \n"
+printf "Message: Command three that is executed: aws configure set default.region us-west-2 \n"
 aws configure set aws_access_key_id AAAAA
 aws configure set aws_secret_access_key AAAAA
 aws configure set default.region us-west-2
 printf "\n"
 
-printf "Localstack is running, now configuring a new S3 Bucket. \n"
-printf "Command one that is executed: 'aws --endpoint-url=http://localhost:4566 s3 mb s3://test-bucket' \n"
+printf "Message: Localstack is running, now configuring a new S3 Bucket. \n"
+printf "Message: Command one that is executed: 'aws --endpoint-url=http://localhost:4566 s3 mb s3://test-bucket' \n"
 aws --endpoint-url=http://localhost:4566 s3 mb s3://test-bucket
 printf "\n"
-printf "Command two that is executed: 'aws --endpoint-url=http://localhost:4566 s3api put-bucket-acl --bucket test-bucket --acl public-read' \n" 
+printf "Message: Command two that is executed: 'aws --endpoint-url=http://localhost:4566 s3api put-bucket-acl --bucket test-bucket --acl public-read' \n" 
 aws --endpoint-url=http://localhost:4566 s3api put-bucket-acl --bucket test-bucket --acl public-read
 printf "\n"
 
-printf "The environment is ready. Time to run the integration tests. \n"
-printf "Command that executed: {dotnet test --logger 'trx;LogFileName=test-results.trx' --collect:'XPlat Code Coverage'} \n"
+printf "Message: The environment is ready. Time to run the integration tests. \n"
+printf "Message: Command that executed: {dotnet test --logger 'trx;LogFileName=test-results.trx' --collect:'XPlat Code Coverage'} \n"
 dotnet test --logger "trx;LogFileName=test-results.trx" --collect:"XPlat Code Coverage"
 printf "\n"
